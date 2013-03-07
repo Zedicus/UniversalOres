26115c0cb719c7fab81b9989b9144d85fcd31817
Zedicus <danbending99@gmail.com>
2013-03-07 17:41:17 +0000

    Base classes

---
+++ core/UniversalOres.java
@@ -0,0 +1,21 @@
+package universalores.core;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class UniversalOres {
+	
+	public static void PreInit(FMLPreInitializationEvent Event) {
+		
+	}
+	
+	public static void Init(FMLInitializationEvent Event) {
+		
+	}
+	
+	public  static void PostInit(FMLPostInitializationEvent Event) {
+		
+	}
+	
+}
